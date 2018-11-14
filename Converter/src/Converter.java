9cd9171ac7755e6ada9fc22620ddb4129254518a
liam.shaw <liam.shaw@MU-C1-06.mullen.pvt.k12.co.us>
2018-11-14 14:53:36 -0700

    started work on binary converter

--- Converter/src/Converter.java
+++ Converter/src/Converter.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class Converter
 	{
-
+		static Scanner userInput = new Scanner(System.in);
 		public static void main(String[] args)
 			{
 				// TODO Auto-generated method stub
@@ -9,10 +9,25 @@ public class Converter
 				// practice pull requests 
 			}
 		public static void promptUser()
-		{
-			Scanner userInput = new Scanner(System.in);
-			System.out.println("What would you like to covert?");
-			String userChoice = userInput.nextLine();
-		}
+			{
+				System.out.println("What would you like to covert?");
+				int userChoice = userInput.nextInt();
+			}
+		public static void convertBases()
+			{
+				System.out.println("What would you like to covert your decimal number into?");
+				System.out.println("(1) Binary");
+				System.out.println("(2) Hexadecimal");
+				int userChoice = userInput.nextInt();
+				if(userChoice == 1)
+					{
+						System.out.println("Please enter a number less than or equal to 128");
+						userChoice = userInput.nextInt();
+					}
+				else if(userChoice == 2)
+					{
+						
+					}
+			}
 
 	}

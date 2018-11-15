433538e7d83f270a7b0e5d4d811d9667b5d0e964
liam.shaw <liam.shaw@MU-C1-06.mullen.pvt.k12.co.us>
2018-11-15 14:16:23 -0700

    restarting

--- Converter/src/Converter.java
+++ Converter/src/Converter.java
@@ -1,7 +1,6 @@
-import java.util.Scanner;
+
 public class Converter
 	{
-		static Scanner userInput = new Scanner(System.in);
 		public static void main(String[] args)
 			{
 				// TODO Auto-generated method stub
@@ -9,27 +8,6 @@ public class Converter
 				// practice pull requests 
 				
 				// please work finally
-			}
-		public static void promptUser()
-			{
-				System.out.println("What would you like to covert?");
-				int userChoice = userInput.nextInt();
-			}
-		public static void convertBases()
-			{
-				System.out.println("What would you like to covert your decimal number into?");
-				System.out.println("(1) Binary");
-				System.out.println("(2) Hexadecimal");
-				int userChoice = userInput.nextInt();
-				if(userChoice == 1)
-					{
-						System.out.println("Please enter a number less than or equal to 128");
-						userChoice = userInput.nextInt();
-					}
-				else if(userChoice == 2)
-					{
-						
-					}
 			}
 
 	}

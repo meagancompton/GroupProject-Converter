3f0ace48f895bd6fd7160fa8116bfd30febe8d03
liam.shaw <liam.shaw@MU-C1-06.mullen.pvt.k12.co.us>
2018-11-15 14:44:57 -0700

    Finished Binary Moving on the Hexadecimal

--- Converter/src/DecimalToBinary.java
+++ Converter/src/DecimalToBinary.java
@@ -1,5 +1,41 @@
-
+import java.util.*;
 public class DecimalToBinary
 	{
-
+		static Scanner userInput = new Scanner(System.in);
+		
+		public static void covertToBinary()
+		{
+			ArrayList <Integer> binaryNumber = new ArrayList <Integer>();
+			boolean running = true;
+			System.out.println("What would you like to covert your decimal number into?");
+			System.out.println("(1) Binary");
+			System.out.println("(2) Hexadecimal");
+			int userChoice = userInput.nextInt();
+			if(userChoice == 1)
+				{
+					System.out.println("Please enter a number less than 128");
+					int userNumber = userInput.nextInt();
+					do
+						{
+							if(userNumber != 0)
+								{
+									binaryNumber.add(userNumber % 2);
+									userNumber = userNumber/2;
+								}
+							else
+								{
+									running = false;
+								}
+						}while(running);
+					Collections.reverse(binaryNumber);
+					for(int i : binaryNumber)
+						{
+							System.out.print(i);
+						}
+				}
+			else if(userChoice == 2)
+				{
+							
+				}
+			}
 	}

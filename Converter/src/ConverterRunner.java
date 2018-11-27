776c77e6eee6e20845be96a613246e6e22ccc262
liam.shaw <liam.shaw@MU-C1-06.mullen.pvt.k12.co.us>
2018-11-27 08:33:50 -0700

    revising our work

--- Converter/src/ConverterRunner.java
+++ Converter/src/ConverterRunner.java
@@ -1,8 +1,9 @@
 
-public class Converter
+public class ConverterRunner
 	{
 		public static void main(String[] args)
 			{
+				System.out.println("Welcome to Blake, Liam, Meagan, and Taylor's Universal Converter");
 				//DecimalToBinary.covertToBinary();
 				WeightConverter.promptUser();
 				//test
